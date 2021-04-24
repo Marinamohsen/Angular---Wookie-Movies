f54b4e23f451278c1907597b434eca67c4a2e5e3
Marinamohsen <marinamohsen164@gmail.com>
2021-04-24 23:13:20 +0200

    Interfaces and Interceptors initialized

---
+++ src/app/interfaces/interface.ts
@@ -0,0 +1,29 @@
+export interface IMovie {
+    backdrop: string;
+    cast: ICast;
+    classification: string;
+    director: string;
+    genres: IGenres;
+    id: string;
+    imdb_rating: number;
+    length: string;
+    overview: string;
+    poster: string;
+    released_on: string;
+    slug: string;
+    title: string;
+
+}
+
+export interface ICast {
+    cast: string;
+}
+
+export interface IGenres {
+    genres: string;
+}
+
+
+
+
+
