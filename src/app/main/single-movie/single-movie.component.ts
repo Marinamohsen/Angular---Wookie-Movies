417c4442c7a6a9d338f5f8875a737c7d5e8742d5
Marinamohsen <marinamohsen164@gmail.com>
2021-04-25 03:31:10 +0200

    movie component updates
    single movie post component and movie component updates

--- src/app/main/single-movie/single-movie.component.ts
+++ src/app/main/single-movie/single-movie.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search-service/search.service';
 import { IMovie } from '../../interfaces/interface'
+import { Route } from '@angular/compiler/src/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies-service/moviesservice.service';
 
 
 @Component({
@@ -10,19 +13,24 @@ import { IMovie } from '../../interfaces/interface'
 })
 export class SingleMovieComponent implements OnInit {
 
-  movies: IMovie[] = []
+  movie: IMovie | null = null
+  id: string = ''
+  
 
-  constructor( private SearchService: SearchService) { }
+  constructor( private moviesService: MoviesService , private route: ActivatedRoute) { }
   
   ngOnInit(): void {
-
-    // this.SearchService.search(keyword: string).subscribe(res => {
-    //   this.movies = res.movies
-
-    //   console.log(this.movies)
-    // });
-  
     
+
+    this.route.params.subscribe(params => {
+      this.id = params['movieId'];
+
+      this.moviesService.getMovies().subscribe(res => {
+        const movies = res?.movies
+        this.movie = movies.find((movie: IMovie) => movie.id === this.id)
+     });
+ 
+    });
 
   }
 }
